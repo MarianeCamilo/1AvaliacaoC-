ac536702ab654daf8c585c9f09967b6dedfb112d
MarianeCamilo <marianecamilo03@gmail.com>
2023-02-13 22:16:00 -0300

    Fim

--- 1Avaliacao.cs
+++ 1Avaliacao.cs
@@ -1,45 +1,58 @@
 using System;
 using System.Text;
 
-
 namespace _1Avaliacao
 {
     class Avaliacao
     {
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines("C:/Users/mariane.costa/Documents/C#/1AvaliavaoC#/entrada.txt", Encoding.UTF8);
+            string[] lines = File.ReadAllLines("C:/Users/mariane.costa/Documents/C#/1AvaliavaoC#/entrada.txt", Encoding.UTF8); // abrindo o arquivo de texto
 
-            Console.WriteLine("lines");
-            Console.WriteLine(lines);
+            Console.WriteLine("lines"); 
+            Console.WriteLine(lines); // imprimindo o arquivo de texto
             foreach (string item in lines)
             {
                 Console.WriteLine(item);
             }
 
-               // Array.ForEach(lines, item => {Console.WriteLine(item);});
+            int[] IntArray = lines.Select(int.Parse).ToArray();    // convertendo o array de string para array de int
+            insertionSort(IntArray);
+            
+            List<int> Collection = IntArray.ToList<int>();
+            
+            string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "C:/Users/mariane.costa/Documents/C#/1AvaliavaoC#/saida.txt")))
+            {
+            foreach (string s in lines)
+                outputFile.WriteLine(s);
+            }
+
         }
 
-        
-    }
 
+            public static void insertionSort(int[] IntArray) 
+            { // Ordenação dos valores        
+            int j, key, i;
+            Console.WriteLine("\nPassos para ordenação: ");
+            for (j = 1; j < IntArray.Length; j++) // for com o tamanho maximo
+            {
+                key = IntArray[j];
+            
+            for (i = j - 1; (i >= 0) && (IntArray[i] > key); i--)
+            {
+                IntArray[i + 1] = IntArray[i];
+            }
+             IntArray[i + 1] = key;
+             Console.WriteLine("Passo para ordenação");
+             foreach(var s in IntArray)
+             {
+                Console.WriteLine(s);
+             }
+        }
 
-
-
-
-
-    class WriteAllLines
-{
-    public static async Task ExampleAsync()
-    {
-        string[] lines =
-        {
-            "1", "5", "8" 
-        };
-
-        await File.WriteAllLinesAsync("entrada.txt", lines);
     }
 }
-
-
 }
